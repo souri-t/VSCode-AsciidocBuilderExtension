2891f67cba6d06a222a84182869a2b0c6794de14
yosuke <>
2024-07-16 03:08:33 +0900

    added a new feature

--- src/extension.ts
+++ src/extension.ts
@@ -15,11 +15,12 @@ export function activate(context: vscode.ExtensionContext) {
 			// Execute Shell Command
 			const { exec } = require('child_process');
 			exec('npm run build', (err: any, stdout: any, stderr: any) => {
+
 				if (err) {
-					console.error(err);
+					vscode.window.showErrorMessage("Failed to build document : " + err);
 					return;
 				}
-				console.log(stdout);
+				vscode.window.showInformationMessage("Success!");
 			});
 		});
 		context.subscriptions.push(disposable);
@@ -32,13 +33,42 @@ export function activate(context: vscode.ExtensionContext) {
 			// Execute Shell Command
 			const { exec } = require('child_process');
 			exec('npm run lint', (err: any, stdout: any, stderr: any) => {
-				if (err) {
-					console.error(err);
-					return;
-				}
-				console.log(stdout);
+	
+				var textMessage = (err) ? err : stdout;
+				const panel = vscode.window.createWebviewPanel(
+					'result', 
+					'result', 
+					vscode.ViewColumn.One, 
+					{}
+				);
+				
+				panel.webview.html = createWebviewContent(textMessage);
 			});
+
 		});
 		context.subscriptions.push(disposable);
 	}
+}
+
+
+/**
+ * Creates the content for a webview with the given message.
+ * Replaces newline characters with HTML line breaks.
+ * 
+ * @param message - The message to be displayed in the webview.
+ * @returns The HTML content for the webview.
+ */
+function createWebviewContent(message: string) {
+	const formattedMessage = message.replace(/\n/g, "<br>");
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Validation Result</title>
+	</head>
+	<body>
+		<h1>Validation Result</h1>
+		<p>${formattedMessage}</p>
+	</body>
+	</html>`;
 }
